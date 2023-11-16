41ad2806bb71488cd0b25d71ec92dd1b5e570d73
masoud <karimi.masoud5494@gmail.com>
2023-11-16 18:36:26 +0330

    upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,32 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.spotless) apply false
+  alias(libs.plugins.com.android.application) apply false
+  alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+  alias(libs.plugins.spotless) apply false
+  alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
 
 subprojects {
-    apply(plugin = "com.diffplug.spotless")
-    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        kotlin {
-            target("**/*.kt", "**/*.kts")
-            targetExclude("$buildDir/**/*.kt", "bin/**/*.kt", "buildSrc/**/*.kt")
-            // by default the target is every '.kt' and '.kts` file in the java sourcesets
-            ktlint() // has its own section below
-                .userData(mapOf("android" to "true"))
-                .editorConfigOverride(
-                    mapOf(
-                        "ktlint_standard_function-naming" to "disabled",
-                    ),
-                )
-        }
-
-      kotlinGradle {
-        target("*.gradle.kts") // default target for kotlinGradle
-        ktlint()
-      }
+  apply(plugin = "com.diffplug.spotless")
+  configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+      target("**/*.kt", "**/*.kts")
+      targetExclude("$buildDir/**/*.kt", "bin/**/*.kt", "buildSrc/**/*.kt")
+      // by default the target is every '.kt' and '.kts` file in the java sourcesets
+      ktlint() // has its own section below
+        .userData(mapOf("android" to "true"))
+        .editorConfigOverride(
+          mapOf(
+            "ktlint_standard_function-naming" to "disabled",
+          ),
+        )
     }
+
+    kotlinGradle {
+      target("*.gradle.kts") // default target for kotlinGradle
+      ktlint()
+    }
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,18 @@
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "Wander List"
 include(":app")
- 
+include(":data:model")
